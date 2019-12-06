5de8de4d7b266ecdb0d4ac2338a97ab2b6172774
Hosni Ghribi <ghribi.hosni7@gmail.com>
2019-12-06 10:53:58 +0100

    Updates

---
+++ precache-manifest.448c1d3948ef4118928aec02a4cbd2b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867c7e87697e0c452a59c3b3489703d9",
+    "url": "/complex-credit-card/index.html"
+  },
+  {
+    "revision": "e1bc87794df28df49d03",
+    "url": "/complex-credit-card/static/css/main.216d77b3.chunk.css"
+  },
+  {
+    "revision": "df00fbd589d24e27baef",
+    "url": "/complex-credit-card/static/js/2.4bbcea6b.chunk.js"
+  },
+  {
+    "revision": "e1bc87794df28df49d03",
+    "url": "/complex-credit-card/static/js/main.d3be6d11.chunk.js"
+  },
+  {
+    "revision": "5786165090919d634252",
+    "url": "/complex-credit-card/static/js/runtime-main.fc53e342.js"
+  },
+  {
+    "revision": "8d796e3ec25a1750f17004c2d50d4b80",
+    "url": "/complex-credit-card/static/media/terre-technologie.8d796e3e.jpg"
+  }
+]);
